01c5710f1cf3232ed4f15138ab3b8e7e7214b307
Waffl3d <zhaoaiden598@gmail.com>
2025-12-13 14:26:28 -0500

    Added basic boxes for login

--- app/signup/page.tsx
+++ app/signup/page.tsx
@@ -1,13 +1,17 @@
 export default function Signup() {
     return (
-        <>
-            <p>Sign Up</p>
-            <input placeholder="Username" type="text"></input>
-            <input placeholder="First Name" type="password"></input>
-            <input placeholder="Last Name" type="password"></input>
-            <input placeholder="Password" type="password"></input>
-            <input placeholder="Confirm Password" type="password"></input>
-            <button>Sign Up</button>
-        </>
+        <div className ="nav-padding">
+            <div className="login-container">
+                <p className="pop-text">Begin your ascension today</p>
+                <input className="signup-inputs" placeholder="Username" type="text"></input>
+                <div className="name-row">
+                <input className="half-input" placeholder="First Name" type="password"></input> 
+                <input className="half-input" placeholder="Last Name" type="password"></input>
+                </div>
+                <input className="signup-inputs" placeholder="Password" type="password"></input>
+                <input className="signup-inputs" placeholder="Confirm Password" type="password"></input>
+                <button className="button">Sign Up</button>
+            </div>
+        </div>
     )
 }
